58b562ed33d3e7e327725abc5e133a88e6dfd16a
KTajerbashi <kamrantajerbashi@gmail.com>
2026-08-27 21:14:33 +0330

    #feat add discount solution and application

---
+++ DiscountSolution/DiscountApp/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DiscountApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
