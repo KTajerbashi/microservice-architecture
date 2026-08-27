7935e7230cdcb00867fb6f93634da272489992d2
KTajerbashi <kamrantajerbashi@gmail.com>
2026-08-27 21:15:02 +0330

    #feat add user solution and application

---
+++ UserSolution/UserApp/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UserApp.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
